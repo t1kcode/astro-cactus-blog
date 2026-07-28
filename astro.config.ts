09c9d5d1dbb14ba7a8495bee927fed6096c74d4b
t1kcode <t1kcode@163.com>
2026-07-29 04:25:11 +0800

    修复

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ import { expressiveCodeOptions, siteConfig } from "./src/site.config";
 export default defineConfig({
 	site: siteConfig.url,
 	image: {
-		domains: ["webmention.io"],
+		domains: ["webmention.io", "cdn.jsdmirror.com"],
 	},
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
